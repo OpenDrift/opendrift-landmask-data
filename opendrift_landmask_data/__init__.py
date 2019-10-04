b13ba6471aca042bb81444b7de3549c61adbb501
Gaute Hope <gaute.hope@met.no>
2019-10-04 14:33:06 +0200

    gshhs: wkbs directly

--- opendrift_landmask_data/__init__.py
+++ opendrift_landmask_data/__init__.py
@@ -1,6 +1,5 @@
 import os.path
 
 shapes = os.path.join(os.path.dirname(__file__), 'shapes') + os.path.sep
-
-GSSHS = [ os.path.join (shapes, 'gshhs_%s_-180.000000E-90.000000N180.000000E90.000000N.wkb' % r) for r in ['c', 'l', 'i', 'h', 'f'] ]
+GSHHS = [ os.path.join (shapes, 'gshhs_%s_-180.000000E-90.000000N180.000000E90.000000N.wkb' % r) for r in ['c', 'l', 'i', 'h', 'f'] ]
 
