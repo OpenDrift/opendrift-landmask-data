8ddcbe1efe1473327cb57a98e7b9180838e2bbde
Gaute Hope <eg@gaute.vetsj.com>
2019-10-04 20:18:16 +0200

    rc2: include wkb with source + py2

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 import setuptools
-from distutils.core import setup
 
-setup (name = 'opendrift_landmask_data',
-       version = '0.1.RC1',
+setuptools.setup (name = 'opendrift_landmask_data',
+       version = '0.1rc2',
        description = 'Joined landmasks for GSHHS features used by cartopy',
        author = 'Gaute Hope',
        author_email = 'gaute.hope@met.no',
        url = 'http://github.com/OpenDrift/opendrift-landmask-data',
-       packages = ['opendrift_landmask_data'],
-       package_data = { 'opendrift_landmask_data': [ 'shapes/*' ] },
+       packages = setuptools.find_packages(),
+       package_data = { '': [ 'shapes/*.wkb' ] },
+       setup_requires = ['setuptools_scm'],
        include_package_data = True
        )
 
