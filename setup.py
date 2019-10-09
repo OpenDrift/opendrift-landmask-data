20ad6dc97cc857100f3440d962be57df0af69acb
Gaute Hope <gaute.hope@met.no>
2019-10-09 10:05:54 +0200

    dont include comp in wheel

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ class BuildPyCommand(BuildPy):
     setuptools.command.build_py.build_py.run(self)
 
 setuptools.setup (name = 'opendrift_landmask_data',
-       version = '0.1rc3',
+       version = '0.1rc4',
        description = 'Joined landmasks for GSHHS features used by cartopy',
        author = 'Gaute Hope',
        author_email = 'gaute.hope@met.no',
        url = 'http://github.com/OpenDrift/opendrift-landmask-data',
-       packages = setuptools.find_packages(),
-       # package_data = { '': [ 'shapes/*.wkb', 'shapes/compressed/*.bz2' ] },
+       packages = setuptools.find_packages(exclude = ['*.compressed']),
+       package_data = { '': [ 'shapes/*.wkb' ] },
        setup_requires = ['setuptools_scm'],
        include_package_data = False,
        cmdclass = { 'build_py': BuildPyCommand }
